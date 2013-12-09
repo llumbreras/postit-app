c5d827bc3a982b17797a114c8fb172c40ed96aa5
L Lumbreras <llumbreras@gmail.com>
2013-12-09 13:16:39 -0800

    Add post validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,8 +4,8 @@ class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
 
-  validates :url, presence: true
-  validates :title, uniqueness: true
-  validates :description, length: {minimum: 5}
+  validates :url, uniqueness: true
+  validates :title, presence: true, length: {minimum: 5}
+  validates :description, presence: true
 
 end
