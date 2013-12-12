9233823eaf7e4cb362b5922e0593862d508512d3
L Lumbreras <llumbreras@gmail.com>
2013-12-11 18:35:21 -0800

    Refactor post controller to remove redundancy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
+  before_action :get_post, only: [:show,:edit,:update]
+
   def index
     @posts = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -23,12 +24,9 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
-
   end
 
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       flash[:notice] = "Post updated successfully."
       redirect_to post_path(@post)
@@ -41,6 +39,10 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:url,:title,:description)
+  end
+
+  def get_post
+    @post = Post.find(params[:id])
   end
 
 end
