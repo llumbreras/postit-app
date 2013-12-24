bf8a4e835d5507437e480d192e386a595c669a6e
L Lumbreras <llumbreras@gmail.com>
2013-12-23 21:54:42 -0800

    Add session actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,21 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.where(username: params[:username]).first
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Welcome #{user.username}, you're now logged in."
+      redirect_to root_path
+    else
+      flash[:error] = "There's an error with your username or password."
+      redirect_to login_path
+    end
   end
 
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "You've logged out."
+    redirect_to root_path
+  end
 end
 
