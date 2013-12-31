aa6a51fa0bd16df0ed35f49dbc0a51e360b65b89
L Lumbreras <llumbreras@gmail.com>
2013-12-31 09:40:00 -0800

    Add voteable module

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,10 @@
 class Post < ActiveRecord::Base
+  include Voteable
+
   belongs_to :author, foreign_key:'user_id', class_name:'User'
   has_many :comments
   has_many :post_categories
   has_many :categories, through: :post_categories
-  has_many :votes, as: :voteable
 
   validates :url, uniqueness: true
   validates :title, presence: true, length: {minimum: 5}
@@ -11,17 +12,6 @@ class Post < ActiveRecord::Base
   
   after_validation :generate_slug!
 
-  def total_votes
-    self.up_votes - self.down_votes
-  end
-
-  def up_votes
-    self.votes.where(vote: true).size
-  end
-
-  def down_votes
-    self.votes.where(vote: false).size
-  end
 
   def to_param
     self.slug
