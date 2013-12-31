ae8b2510aad61794a0df45a146a365fc8aa31193
L Lumbreras <llumbreras@gmail.com>
2013-12-31 09:55:53 -0800

    Modify module to use concerns

--- lib/voteable.rb
+++ lib/voteable.rb
@@ -1,3 +1,27 @@
+# Using Concerns
+
+module Voteable
+  extend ActiveSupport::Concern
+
+  included do
+    has_many :votes, as: :voteable
+  end
+
+  def total_votes
+    self.up_votes - self.down_votes 
+  end 
+  def up_votes 
+    self.votes.where(vote: true).size 
+  end 
+  def down_votes 
+    self.votes.where(vote: false).size 
+  end
+
+end
+
+# Using metaprogramming
+=begin
+
 module Voteable
   def self.included(base)
     base.send(:include, InstanceMethods)
@@ -25,3 +49,4 @@ module Voteable
     end
   end
 end
+=end
