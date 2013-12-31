ee5ed35be5293dc1214eb9b6c495d91edbda5285
L Lumbreras <llumbreras@gmail.com>
2013-12-31 11:06:04 -0800

    Add sluggable module

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
   include Voteable
+  include Sluggable
 
   belongs_to :author, foreign_key:'user_id', class_name:'User'
   has_many :comments
@@ -10,37 +11,6 @@ class Post < ActiveRecord::Base
   validates :title, presence: true, length: {minimum: 5}
   validates :description, presence: true
   
-  after_validation :generate_slug!
+  sluggable_column :title
 
-
-  def to_param
-    self.slug
-  end
-
-  def generate_slug!
-    the_slug = to_slug(self.title)
-    post = Post.find_by(slug: the_slug)
-    count = 2
-    while post && post != self
-      the_slug = append_suffix(the_slug, count)
-      post = Post.find_by(slug: the_slug)
-      count += 1
-    end
-    self.slug = the_slug.downcase
-  end
-
-  def append_suffix(str, count)
-    if str.split('-').last.to_i !=0
-      return str.split('-').slice(0...-1).join('-') + "-" + count.to_s
-    else
-      return str + "-" + count.to_s
-    end
-  end
-
-  def to_slug(title_name)
-    str = title_name.strip
-    str.gsub!(/\s*[^A-Za-z0-9]\s*/, '-')
-    str.gsub!(/-+/, '-')
-    str.downcase
-  end
 end
