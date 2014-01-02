bf52b06fb204171fc37b73c092615771793a4cbf
L Lumbreras <llumbreras@gmail.com>
2014-01-02 10:54:25 -0800

    Add role to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,4 +20,13 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def require_admin
+    access_denied unless logged_in? and current_user.admin?
+  end
+
+  def access_denied
+    flash[:error] = "You must be an admin to perform this action"
+    redirect_to root_path
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140101061859) do
+ActiveRecord::Schema.define(version: 20140102030928) do
 
   create_table "categories", force: true do |t|
     t.string "name"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20140101061859) do
     t.string "password_digest"
     t.string "slug"
     t.string "time_zone"
+    t.string "role"
   end
 
   create_table "votes", force: true do |t|
